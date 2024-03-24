3339ce48871996cb40b98abcb6bedca69c8ec63d
YUAN-TSUNG <satoshi.liu@icloud.com>
2024-03-24 16:54:01 +0900

    🤔 base changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/p3r-site-copy/',
   resolve: {
     alias: {
       '@/': '/src/',
